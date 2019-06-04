bda0e5401459f4a0ab9f3cb9bcdea98228ee4889
ItzSpam <44649434+ItzSpam@users.noreply.github.com>
2019-06-04 04:27:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '=';
+
+ 
+ 
+ 
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+
+
 client.on('ready',async () => {
   let GUILDID = '441998122827120640'; // اي دي السيرفر
   let CHANNELID = '583335594109698085'; // اي دي الروم
