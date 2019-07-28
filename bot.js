9a5be9ddd2b87419b43075c2d29db935050c81b7
ItzSpam <44649434+ItzSpam@users.noreply.github.com>
2019-07-28 18:36:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('ready', () => {
 
 client.on('ready',async () => {
   let GUILDID = '601018827702140948'; // اي دي السيرفر
-  let CHANNELID = '603601468930523137'; // اي دي الروم
+  let CHANNELID = '603843418333904896'; // اي دي الروم
   voiceStay(GUILDID, CHANNELID);
   function voiceStay(guildid, channelid) {
     if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
